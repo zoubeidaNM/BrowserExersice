4daed16d171cd0135fa3593bd725f541bf66ce29
zoubeidaNM <zoumenz@yahoo.com>
2017-10-26 12:10:42 -0400

    Added an if statement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,27 +11,33 @@ public class Main {
 
         Browser browser = new Browser();
         System.out.println("enter browser name?");
-        browser.setName(keyboard.nextLine());
-        do {
+        String browserName = keyboard.nextLine();
 
-            if (tabAns.equalsIgnoreCase("y")) {
-                Tab tab = new Tab();
-                System.out.println("enter a tab title?");
-                tab.setTitle(keyboard.nextLine());
+        if(!browserName.equalsIgnoreCase("Internet Explorer")) {
+            browser.setName(browserName);
+            do {
 
-                System.out.print("enter tab url?");
-                tab.setUrl(keyboard.nextLine());
+                if (tabAns.equalsIgnoreCase("y")) {
+                    Tab tab = new Tab();
+                    System.out.println("enter a tab title?");
+                    tab.setTitle(keyboard.nextLine());
 
-                browser.addTab(tab);
-            }
+                    System.out.print("enter tab url?");
+                    tab.setUrl(keyboard.nextLine());
+
+                    browser.addTab(tab);
+                }
 
                 System.out.println("do you want to enter other tab? (y/n)");
                 tabAns = keyboard.next();
                 keyboard.nextLine();
 
-        } while (!tabAns.equalsIgnoreCase("n"));
+            } while (!tabAns.equalsIgnoreCase("n"));
 
-        System.out.println(browser.toString());
+            System.out.println(browser.toString());
+        }else {
+        System.out.println("Use a different browser!");
+        }
 
 
     }
